cdc350850f2c03fafa3ab85d988d8056f628d390
Deepesh Reddy <deepeshreddy03@gmail.com>
2025-06-05 22:55:18 +0530

    props practical

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,12 +7,24 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    name:'Reddy',
+    age:22,
+    address:{
+      city:'Tirupati',
+      state:'Andhra Pradesh',
+      country:'India'
+    }
+  }
+  let newArray = [1,2,3,4,5,6,7,8,9]
+
+  const url = "https://i.pcmag.com/imagery/articles/03xdeDG4m4n0gJG3CbFNIgm-17..v1661444591.png"
   return (
     <>
       <h1 className = 'text-3xl bg-green-500 p-3 rounded-md' >Vite with tailwind</h1>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card username = "Deepesh" link = {url} myArr = {newArray} />
+      <Card username="json" post="Staff Engg." />
+      <Card />
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({username = "Sai",post = "Not Assigned Yet" , link = "Image", myArr = {}}) => {
+  // console.log(props);  
   return (
-    <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl bg-gray-300 m-1">
+    <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl bg-gray-300 mt-1">
   <div>
-    <img className="size-48 shadow-xl rounded-md bg " alt="" src="https://i.pcmag.com/imagery/articles/03xdeDG4m4n0gJG3CbFNIgm-17..v1661444591.png" />
+    <img className="size-48 shadow-xl rounded-md bg " alt="" src={link} />
   </div>
   <div className="flex items-center md:items-start bg-sky-300">
-    <span className="text-2xl font-medium">className Warfare</span>
-    <span className="font-medium text-sky-500">The Anti-Patterns</span>
+    <span className="text-2xl font-medium">{username || "" } </span>
+    <span className="font-medium text-sky-500">{post}</span>
     <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-      <span>No. 4</span>
+      <span>No. {myArr[5]}</span>
       <span>·</span>
       <span>2025</span>
     </span>
