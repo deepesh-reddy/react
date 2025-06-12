53d47d9b4f3b341a8fd8575c901e34d170fce540
Deepesh Reddy <deepeshreddy03@gmail.com>
2025-06-12 23:50:03 +0530

    react-router-2

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter,createRoutesFromElements, Route,RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Company from './components/Company/Company.jsx'
 import Contact from './components/ContactUs/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,10 @@ const router = createBrowserRouter(
         <Route path='company' element={<Company/>}/>
       </Route>
       <Route path='contact' element={<Contact/>}/>
+      <Route path='user/' element={<User/>}>
+        <Route path=':userid' element={<User/>}/>
+      </Route>
+      <Route path='github' element={<Github/>}/>
     </Route>
   )
 )
